39a4daa7f2a5c32991d7646304b787f70a626337
Hardik <hardikrajpal@cse.iitb.ac.in>
2021-10-15 22:39:47 +0530

    published boolean in jc

--- junglechat/models.py
+++ junglechat/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class ChatSnippet(models.Model):
     snippet = models.TextField()
+    published = models.BooleanField(default=True)
     def __str__(self):
         return self.snippet.split('H#')[1]
