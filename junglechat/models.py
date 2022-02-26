fb0070c723f3d207ab9c540aa53862f0cbffb7aa
Hardik <hardikrajpal@cse.iitb.ac.in>
2022-02-26 13:52:21 +0530

    usrtkn

--- junglechat/models.py
+++ junglechat/models.py
@@ -1,10 +1,10 @@
 
 from django.db import models
 from django.utils import timezone
-# class UserToken(models.Model):
-#     dateMade=models.DateField(auto_created=True,default=timezone.now)
-#     def __str__(self):
-#         return str(self.dateMade)
+class UserToken(models.Model):
+    dateMade=models.DateField(auto_created=True,default=timezone.now)
+    def __str__(self):
+        return str(self.dateMade)
 class Tag(models.Model):
     title = models.CharField(max_length=30)
     def __str__(self):
